c6a7f82ff41ffff2fe3f7baa22a088be9f67617e
Dylan McKay <dylanmckay34@gmail.com>
2016-10-02 23:28:28 +1300

    Make the VGA interface nicer

--- src/entry.rs
+++ src/entry.rs
@@ -3,6 +3,8 @@
 #![feature(link_args)]
 #![feature(asm)]
 #![feature(naked_functions)]
+#![feature(conservative_impl_trait)]
+
 #![no_std]
 
 pub mod libc;
@@ -10,11 +12,10 @@ pub mod vga;
 
 #[no_mangle]
 pub extern "C" fn kernel_main(_argc: isize, _argv: *const *const u8) -> isize {
-    use vga;
+    let mut buffer = vga::Buffer::new();
 
-    let mut vmem = unsafe { core::slice::from_raw_parts_mut(vga::MEMORY, 80*25) };
     loop {
-        for cell in vmem.iter_mut() {
+        for cell in buffer.cells_mut() {
             cell.character = 'a' as u8;
         }
     }
--- src/vga.rs
+++ src/vga.rs
@@ -1,6 +1,10 @@
+use core::slice;
+use core::ops;
+
 pub const MEMORY: *mut Cell = 0xb8000 as _;
 
 #[repr(u8)]
+#[derive(Clone)]
 pub enum Color {
     Black = 0,
     Blue = 1,
@@ -21,8 +25,71 @@ pub enum Color {
 }
 
 #[repr(C, packed)]
+#[derive(Clone)]
 pub struct Cell {
     pub character: u8,
     pub color: Color,
+}
+
+/// A VGA buffer.
+pub struct Buffer
+{
+    pub width: usize,
+    pub height: usize,
+
+    pub memory: &'static mut [Cell],
+}
+
+impl Buffer
+{
+    pub fn new() -> Self {
+        let width = 80;
+        let height = 25;
+
+        let memory = unsafe {
+            slice::from_raw_parts_mut(MEMORY, width*height)
+        };
+
+        Buffer {
+            width: width,
+            height: height,
+            memory: memory,
+        }
+    }
+
+    pub fn clear(&mut self, cell: Cell) {
+        for c in self.memory.iter_mut() {
+            *c = cell.clone();
+        }
+    }
+
+    pub fn cells<'a>(&'a self) -> impl Iterator<Item=&'a Cell> {
+        self.memory.iter()
+    }
+
+    pub fn cells_mut<'a>(&'a mut self) -> impl Iterator<Item=&'a mut Cell> {
+        self.memory.iter_mut()
+    }
+}
+
+impl ops::Index<(usize, usize)> for Buffer
+{
+    type Output = Cell;
+
+    fn index(&self, (x,y): (usize, usize)) -> &Cell {
+        &self.memory[calculate_index(x, y, self.width)]
+    }
+}
+
+impl ops::IndexMut<(usize, usize)> for Buffer
+{
+    fn index_mut(&mut self, (x,y): (usize, usize)) -> &mut Cell {
+        &mut self.memory[calculate_index(x, y, self.width)]
+    }
+}
+
+/// Calculates the index of a cell in a given VGA buffer.
+fn calculate_index(x: usize, y: usize, width: usize) -> usize {
+    (y * width) + x
 }
 
